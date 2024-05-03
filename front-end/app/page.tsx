72120c3539925d1414c98b0678fb0d4dd8ee3ffb
satsuki minamoto <21520474@gm.uit.edu.vn>
2024-05-03 11:30:59 +0700

    create auth context

--- front-end/app/page.tsx
+++ front-end/app/page.tsx
@@ -1,16 +1,20 @@
 "use client"
 import Navbar from "@/components/NavBar";
 import HomePage from "./HomePage";
-
 import Search from "@/app/Search/page";
 import UserProfile from "./UserProfile/page";
 import TopUser from "./TopUser";
 import EditProfile from "../components/EditProfile";
 import { RecipesProvider } from "@/context/RecipesContext";
+import { AuthProvider } from "@/context/AuthContext";
 
 export default function Home() {
   return (
+ <AuthProvider>
     <main className="">
+
         <HomePage/>
     </main>
+</AuthProvider>
+
   )};
