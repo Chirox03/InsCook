3204657df1f6bfb979a181c4cc032d0948aaf7e4
trhuuloc04 <trhuuloc04@gmail.com>
2024-05-29 11:08:03 +0700

    follow-button

--- front-end/app/UserProfile/[uid]/page.tsx
+++ front-end/app/UserProfile/[uid]/page.tsx
@@ -105,7 +105,9 @@ function UserProfile({ params }: { params: { uid: string }}) {
                   <Link href={`/Edit/${params.uid}`}>Edit</Link>
                 </button>
               )}
-              <FollowButton/>
+              {!yourProfile && (
+                <FollowButton/>
+              )}
               <button type="button" className="mb-0 text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 font-medium rounded-lg text-xs px-5 py-2 me-2  dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-600 dark:focus:ring-gray-700">
                 <Link href={`Storage/${params.uid}`}>Storage</Link>
               </button>
